7d128da44d7eb13f2905371e02534ae97b85c006
CupidJay <34046764+CupidJay@users.noreply.github.com>
2021-03-25 08:31:15 +0800

    ADD MoCo part pre-training code

---
+++ moco/finetune_cub.sh
@@ -0,0 +1,9 @@
+python main_moco_finetune.py \
+  -a resnet50 \
+  --lr 0.03 \
+  --batch-size 128 --epochs 200 \
+  --input-size 224 \
+  --dist-url 'tcp://localhost:10004' --multiprocessing-distributed --world-size 1 --rank 0 \
+  --gpus 12,13,14,15 \
+  --mlp --moco-t 0.2 --moco-k 4096 --aug-plus --cos \
+  /opt/caoyh/datasets/cub200
